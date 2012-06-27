4f73697e3d417ec9cc5df23881d3ceb83f2e19a2
caioarcade <caioarcade@b487b043-83e6-6c4b-b40c-f9aefa2967bf>
2012-06-27 17:51:36 +0000

    Adicionado as abas PHP e C#

--- GeradorClasse.Programa/Form1.Designer.cs
+++ GeradorClasse.Programa/Form1.Designer.cs
@@ -40,15 +40,15 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
             this.btnGerarEntidades = new System.Windows.Forms.Button();
             this.txtPrefixo = new System.Windows.Forms.TextBox();
             this.ckbPrefixo = new System.Windows.Forms.CheckBox();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.groupBox1.SuspendLayout();
-            this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
+            this.tabControl2.SuspendLayout();
+            this.tabPage4.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -166,81 +166,79 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Servidor:";
             // 
-            // tabControl1
+            // tabControl2
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(336, 19);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(593, 334);
-            this.tabControl1.TabIndex = 1;
+            this.tabControl2.Controls.Add(this.tabPage3);
+            this.tabControl2.Controls.Add(this.tabPage4);
+            this.tabControl2.Location = new System.Drawing.Point(336, 16);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(593, 337);
+            this.tabControl2.TabIndex = 2;
             // 
-            // tabPage1
+            // tabPage3
             // 
-            this.tabPage1.Controls.Add(this.btnGerarEntidades);
-            this.tabPage1.Controls.Add(this.txtPrefixo);
-            this.tabPage1.Controls.Add(this.ckbPrefixo);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(585, 308);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Entidades";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(585, 311);
+            this.tabPage3.TabIndex = 0;
+            this.tabPage3.Text = "PHP";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.btnGerarEntidades);
+            this.tabPage4.Controls.Add(this.txtPrefixo);
+            this.tabPage4.Controls.Add(this.ckbPrefixo);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(585, 311);
+            this.tabPage4.TabIndex = 1;
+            this.tabPage4.Text = "C#";
+            this.tabPage4.UseVisualStyleBackColor = true;
             // 
             // btnGerarEntidades
             // 
-            this.btnGerarEntidades.Location = new System.Drawing.Point(355, 20);
+            this.btnGerarEntidades.Location = new System.Drawing.Point(353, 18);
             this.btnGerarEntidades.Name = "btnGerarEntidades";
             this.btnGerarEntidades.Size = new System.Drawing.Size(75, 23);
-            this.btnGerarEntidades.TabIndex = 2;
+            this.btnGerarEntidades.TabIndex = 8;
             this.btnGerarEntidades.Text = "Gerar";
             this.btnGerarEntidades.UseVisualStyleBackColor = true;
-            this.btnGerarEntidades.Click += new System.EventHandler(this.btnGerarEntidades_Click);
             // 
             // txtPrefixo
             // 
-            this.txtPrefixo.Location = new System.Drawing.Point(238, 22);
+            this.txtPrefixo.Location = new System.Drawing.Point(236, 20);
             this.txtPrefixo.Name = "txtPrefixo";
             this.txtPrefixo.Size = new System.Drawing.Size(72, 20);
-            this.txtPrefixo.TabIndex = 1;
+            this.txtPrefixo.TabIndex = 7;
             // 
             // ckbPrefixo
             // 
             this.ckbPrefixo.AutoSize = true;
-            this.ckbPrefixo.Location = new System.Drawing.Point(18, 22);
+            this.ckbPrefixo.Location = new System.Drawing.Point(16, 20);
             this.ckbPrefixo.Name = "ckbPrefixo";
             this.ckbPrefixo.Size = new System.Drawing.Size(213, 17);
-            this.ckbPrefixo.TabIndex = 0;
+            this.ckbPrefixo.TabIndex = 6;
             this.ckbPrefixo.Text = "Utilizar prefixo para chaves estrangeiras";
             this.ckbPrefixo.UseVisualStyleBackColor = true;
-            this.ckbPrefixo.CheckedChanged += new System.EventHandler(this.ckbPrefixo_CheckedChanged);
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(585, 308);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(941, 373);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.tabControl2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Class Generator 1.0";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
+            this.tabControl2.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -259,12 +257,12 @@
         private System.Windows.Forms.Button btnTestar;
         private System.Windows.Forms.ComboBox cbxDatabase;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
         private System.Windows.Forms.Button btnGerarEntidades;
         private System.Windows.Forms.TextBox txtPrefixo;
         private System.Windows.Forms.CheckBox ckbPrefixo;
-        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
--- GeradorClasse.Programa/Form1.cs
+++ GeradorClasse.Programa/Form1.cs
@@ -63,6 +63,8 @@ namespace GeradorClasse.Programa
             return database.GetDatabases(txtServidor.Text, txtUsuario.Text, txtSenha.Text);
         }
 
+       
+
 
 
 
